443b7c01ee8c2568b3380e385c9a74782c1cbb27
Niklas <33700501+NIKL45@users.noreply.github.com>
2021-01-23 12:29:51 +0100

    solved path issue

--- oui.sh
+++ oui.sh
@@ -2,10 +2,11 @@
 
 MAC="$(echo $1 | sed 's/ //g' | sed 's/-//g' | sed 's/://g' | cut -c1-6)";
 
-result="$(grep -i -A 4 ^$MAC /mac/oui.txt)";
+result="$(grep -i -A 4 ^$MAC ~/mac/oui.txt)";
 
 if [ "$result" ]; then
     echo "For the MAC $1 the following information is found:"
+    echo ""
     echo "$result"
 else
     echo "MAC $1 is not found in the database."
